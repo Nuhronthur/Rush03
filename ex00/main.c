e345359f90498ac9bc1217debeb8ac7e582a9ef7
Jonathan Vella <jonvella@c2r1p6.42nice.fr>
2022-02-12 18:10:15 +0100

    nouveau main' git push origin master

--- ex00/main.c
+++ ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: jonvella <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 15:24:23 by jonvella          #+#    #+#             */
-/*   Updated: 2022/02/12 17:22:12 by jonvella         ###   ########.fr       */
+/*   Updated: 2022/02/12 18:07:22 by jonvella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define BUF_SIZE 4096
+#define BUF_SIZE 691
+int	ft_atoi(char *str);
 
 void	ft_putchar(char c)
 {
@@ -39,15 +40,23 @@ void	rtu(char *str, int nb)
 			i++;
 		}
 	i++;
-
 	while (str[i] != '\n')
 	{
 		while (str[i] == ' ')
-		{
 			i++;
-		}
 		ft_putchar(str[i]);
 		i++;
 	}
 }
 
+int main(int ac, char **av)
+{
+	char res[BUF_SIZE];
+	int	fd;
+	int r;
+
+	fd = open("numbers.dict.txt", O_RDONLY);
+	r = read(fd, res, BUF_SIZE); 
+//	printf("%s", &res);
+	rtu(res, ft_atoi(av[1]));
+}
